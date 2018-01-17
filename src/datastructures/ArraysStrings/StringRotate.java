ec5f249a95e08143c08d49826fdcdfea82a59ca7
Vidhi Sharma <vidhi.sharma@sjsu.edu>
2018-01-17 14:34:06 -0800

    Create StringRotate.java

---
+++ src/datastructures/ArraysStrings/StringRotate.java
@@ -0,0 +1,15 @@
+/*If s2 is the rotation of s1 .
+Assumption: isSubstring method is already present.
+*/
+
+
+
+boolean isRotateion(String s1,String s2){
+int len=s1.length();
+if(len == s2.length() && len >0){
+  String s1s1=s1+s1;
+  return isSubstring(s1s1,s2);
+}
+return false;
+
+}
