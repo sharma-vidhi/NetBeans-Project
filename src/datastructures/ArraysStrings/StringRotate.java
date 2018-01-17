160598eb361330ad176cb99df5e7d52fbdc26d75
Vidhi Sharma <vidhi.sharma@sjsu.edu>
2018-01-17 15:03:44 -0800

    Update StringRotate.java

--- src/datastructures/ArraysStrings/StringRotate.java
+++ src/datastructures/ArraysStrings/StringRotate.java
@@ -8,7 +8,7 @@ if(large.indexOf(small)>0){ return true;}
   else {return false;}
 }
 
-boolean isRotation(String s1,String s2){
+public static boolean isRotation(String s1,String s2){
 int len=s1.length();
 if(len == s2.length() && len >0){
   String s1s1=s1+s1;
